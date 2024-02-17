5eee21bef20d37455cc1eb8633b6ea99660ff2e7
Italo Alves <contato.italo1020@gmail.com>
2024-02-17 11:37:52 -0300

    add module ngx angular analytics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { RouterModule } from '@angular/router';
+import { NgxGoogleAnalyticsModule } from 'ngx-google-analytics';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CategoriasComponent } from './categorias/categorias.component';
@@ -17,6 +18,7 @@ import { ProdutosComponent } from './produtos/produtos.component';
   ],
   imports: [
     BrowserModule,
+    NgxGoogleAnalyticsModule.forRoot('G-J2JEDKZNVN'),
     AppRoutingModule,
     RouterModule
   ],
